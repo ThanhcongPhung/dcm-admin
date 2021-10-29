a59b1a57ec41031445029c078ca7a4dc5a2a9e9e
Henry <thanhpv3380@gmail.com>
2021-10-29 08:52:04 +0700

    remove fake auth api

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,20 +1,6 @@
 import api from './api';
 
 export async function verify(token) {
-  const today = new Date();
-  return {
-    data: {
-      status: 1,
-      result: {
-        accessToken: token,
-        email: `${today.getTime()}@vbee.vn`,
-        id: '610763ccb1504e39f7f2f2c5',
-        name: 'Thu Hương',
-        ssoUserId: 'd28bc305-6eb6-4f5a-b052-39d403101533',
-        createdAt: '2021-08-09T02:35:34.409Z',
-      },
-    },
-  };
   try {
     const response = await api({
       method: 'GET',
